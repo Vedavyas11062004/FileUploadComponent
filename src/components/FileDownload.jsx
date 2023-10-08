44539f62b961c6efbca3b57afeb4e425b5bc5fcf
vedavyas <vedavyas14042003@gmail.com>
2023-10-08 22:11:23 +0530

    added svg image

--- src/components/FileDownload.jsx
+++ src/components/FileDownload.jsx
@@ -5,7 +5,7 @@ export default function FileDownload() {
   return (
     <div className="downLoadDiv">
       <h2>Your outreach sheet is Ready</h2>
-      <img src="./csv 1.png" alt="img.." />
+      <img src="./csv 1.svg" alt="img.." />
       <span>testsheet.csv - 12mo</span>
       <button>Download <img src="./download.svg" alt="downloadimg..."/> </button>
     </div>
