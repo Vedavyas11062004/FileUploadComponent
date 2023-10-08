1de16facf2c9cb4e1d7e6b4c697d787b7725d15c
vedavyas <vedavyas14042003@gmail.com>
2023-10-08 23:47:28 +0530

    Added changes

--- src/components/FileDownload.jsx
+++ src/components/FileDownload.jsx
@@ -7,7 +7,12 @@ export default function FileDownload() {
       <h2>Your outreach sheet is Ready</h2>
       <img src="./csv 1.svg" alt="img.." />
       <span>testsheet.csv - 12mo</span>
-      <button>Download <img src="./download.svg" alt="downloadimg..."/> </button>
+      <button>
+        Download <img src="./download.svg" alt="downloadimg..." />{" "}
+      </button>
+      <div className="buttomIcon">
+        <img src="./Tick.svg" alt="img4.." /> 100%-2039/29220
+      </div>
     </div>
   );
 }
