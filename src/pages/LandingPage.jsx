6a7f10c44d0b5701a065de64ff9cb6229c2d618d
vedavyas <vedavyas14042003@gmail.com>
2023-10-10 19:57:09 +0530

    Added loading to genrate Image

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import Navbar from "../components/Navbar";
 import "../styles/landingPageStyles.css";
-import FileUploader from "../components/FileUploader";
+// import FileUploader from "../components/FileUploader";
 import BottomLayout from "../components/BottomLayout";
+import "../styles/fileUploadStyles.css";
+import "react-toastify/dist/ReactToastify.css";
+import LoadingAnimation from "../components/LoadingAnimation";
+// import FileDownload from "./FileDownload";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   const [selectedOption, setSelectedOption] = useState("Cold Outreach");
@@ -17,6 +22,79 @@ function LandingPage() {
     console.log(showOptions);
     setShowOptions(!showOptions);
   };
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [percentage, setPercentage] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let interval;
+    interval = setInterval(() => {
+      setPercentage((prevPercentage) => {
+        const newPercentage = prevPercentage + 1;
+        return newPercentage <= 100 ? newPercentage : 100;
+      });
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleButtonClick = () => {
+    document.getElementById("fileInput").click();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    setSelectedFile(file);
+    setPreview(file);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+    setPreview(file);
+  };
+
+  const setPreview = (file) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      setPreviewUrl(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleRemove = () => {
+    setSelectedFile(null);
+    setPreviewUrl(null);
+  };
+
+  const handlePopUp1 = () => {
+    navigate("/download");
+    document.getElementById("popup1").click();
+    const timer = setTimeout(() => {
+      document.getElementById("dismissBtn").click();
+      setIsOpen(true);
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (selectedFile) {
+      console.log("File uploaded:", selectedFile);
+    }
+    handlePopUp1();
+  };
+
   return (
     <div>
       <Navbar />
@@ -33,7 +111,101 @@ function LandingPage() {
         </div>
         <div className="bottomPart">
           <div className="bottomPartLeft">
-            <FileUploader />
+            {/* FILE UPLOADER */}
+            <div>
+              {!isOpen && (
+                <div className="outerContainer">
+                  <form onSubmit={handleFormSubmit}>
+                    <input
+                      type="file"
+                      id="fileInput"
+                      accept=".csv"
+                      onChange={handleFileUpload}
+                    />
+                    <button
+                      type="submit"
+                      disabled={!selectedFile}
+                      className="button"
+                    >
+                      Upload your leads
+                    </button>
+                  </form>
+                  <div
+                    onDrop={handleDrop}
+                    onDragOver={handleDragOver}
+                    className="fileUploadDiv"
+                  >
+                    {previewUrl ? (
+                      <div className="innerFileUpload">
+                        <button onClick={handleRemove} className="crossBtn">
+                          <img src="./Vector.svg" alt="img2.."></img>
+                        </button>
+                        <img
+                          className="previewImage"
+                          src="./csv 1.svg"
+                          alt="Preview"
+                          style={{ maxWidth: "80%", maxHeight: "160px" }}
+                        />
+                      </div>
+                    ) : (
+                      <p onClick={handleButtonClick} className="dropBoxText">
+                        Click to browse or drag and drop your files
+                      </p>
+                    )}
+                  </div>
+                  <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog modal-dialog-centered">
+                      <div className="modal-content modalBox">
+                        <div className="modal-header">
+                          <h3
+                            className="modal-title fs-5"
+                            id="exampleModalLabel"
+                          >
+                            Generating Content
+                          </h3>
+                          <p>COLD OUTREACH SENTENCES...</p>
+                        </div>
+                        <div className="modal-body">
+                          <LoadingAnimation />
+                          <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                            id="dismissBtn"
+                          >
+                            cancel
+                            <img src="./Vector.svg" alt="" />
+                          </button>
+                        </div>
+                        <div className="modal-footer modalBox2">
+                          <img src="./Tick.svg" alt="img4.." /> {percentage}%-
+                          {parseInt((percentage / 100) * 29220)}/29220
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="popupBtns">
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                      id="popup1"
+                      style={{ display: "none" }}
+                    >
+                      Launch static backdrop modal
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+            {/* FILE UPLOADER */}
           </div>
           <div className="bottomPartRight">
             <div className="leftOne">
@@ -62,7 +234,7 @@ function LandingPage() {
                     onClick={toggleOptions}
                   >
                     {selectedOption || "Select an option"}
-                    <img src="./Icon2.svg" alt="Icon" className="IconImg"/>
+                    <img src="./Icon2.svg" alt="Icon" className="IconImg" />
                   </div>
                   {showOptions && (
                     <div className="custom-options">
@@ -97,7 +269,7 @@ function LandingPage() {
               </div>
             </div>
             <div className="rightOne">
-              <button>
+              <button onClick={handlePopUp1}>
                 Generate content <img src="./thunder.svg" alt="img.."></img>{" "}
               </button>
             </div>
